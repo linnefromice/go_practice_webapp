acba7aa741642d381890222370e2297df187c628
linnefromice <arata05.rocketstart@gmail.com>
2020-03-08 23:03:24 +0900

    modify index.html

--- go_webapp_01/main.go
+++ go_webapp_01/main.go
@@ -8,11 +8,11 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 
-	title := "Hello World!"
-	subtitle := "Hello go-gin!"
+	content := "Hello World!"
+	subContent := "Hello go-gin!"
 
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "index.html", gin.H{"title": title, "subtitle": subtitle})
+		ctx.HTML(200, "index.html", gin.H{"content": content, "subContent": subContent})
 	})
 
 	router.Run()
