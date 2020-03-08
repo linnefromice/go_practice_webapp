52d259f0eb0af50c57d603756db43115fdf5e238
linnefromice <arata05.rocketstart@gmail.com>
2020-03-08 23:07:08 +0900

    add new page

--- go_webapp_01/main.go
+++ go_webapp_01/main.go
@@ -14,6 +14,12 @@ func main() {
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(200, "index.html", gin.H{"content": content, "subContent": subContent})
 	})
+	router.GET("/character", func(ctx *gin.Context) {
+		ctx.HTML(200, "character_page.html", gin.H{})
+	})
+	router.GET("/movie", func(ctx *gin.Context) {
+		ctx.HTML(200, "movie_page.html", gin.H{})
+	})
 
 	router.Run()
 }
