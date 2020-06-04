88ccf7f4438f831f60acb775270a927a4c2e8ef6
linnefromice <linnefromice@gmail.com>
2020-06-04 13:57:43 +0900

    update go_webapp_02 - add view2

--- go_webapp_02/main.go
+++ go_webapp_02/main.go
@@ -17,9 +17,26 @@ func htmlHandler0(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func htmlHandler1(w http.ResponseWriter, r *http.Request) {
+	// テンプレートをparse
+	t := template.Must(template.ParseFiles("templates/template001.html.tpl"))
+	m := map[string]int{
+		"key1": 101,
+		"key2": 202,
+		"key3": 303,
+		"key4": -404,
+	}
+
+	// テンプレートを描画
+	if err := t.ExecuteTemplate(w, "template001.html.tpl", m); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	log.Println("Launching...")
 	http.HandleFunc("/page0", htmlHandler0)
+	http.HandleFunc("/page1", htmlHandler1)
 
 	// サーバーを起動
 	http.ListenAndServe(":8080", nil)
---
+++ go_webapp_02/templates/template001.html.tpl
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<body>
+    key1: {{.key1}}, key2: {{.key2}}, key3: {{.key3}}, key4: {{.key4}}
+</body>
+</html>
