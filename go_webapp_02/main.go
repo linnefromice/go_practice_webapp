1d336887d7b16d7caec5cbeb9d2d9f53342bf285
linnefromice <linnefromice@gmail.com>
2020-06-04 14:40:14 +0900

    update go_webapp_02 - add view4

--- go_webapp_02/main.go
+++ go_webapp_02/main.go
@@ -4,6 +4,9 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
+	"fmt"
+	"strings"
 )
 
 func htmlHandler0(w http.ResponseWriter, r *http.Request) {
@@ -48,11 +51,31 @@ func htmlHandler2(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func htmlHandler3(w http.ResponseWriter, r *http.Request) {
+	funcMap := template.FuncMap{
+		"samplefunc1": func() string { return time.Now().String() },
+		"samplefunc2": func(src string) string { return fmt.Sprintf("[$$ %s $$]", src)},
+		"samplefunc3": strings.ToUpper,
+	}
+	// テンプレートをparse
+	t := template.Must(template.New("t").Funcs(funcMap).ParseFiles("templates/template003.html.tpl"))
+	m := map[string]string {
+		"msg1": "golang is programming language.",
+		"msg2": "hello template app.",
+	}
+
+	// テンプレートを描画
+	if err := t.ExecuteTemplate(w, "template003.html.tpl", m); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	log.Println("Launching...")
 	http.HandleFunc("/page0", htmlHandler0)
 	http.HandleFunc("/page1", htmlHandler1)
 	http.HandleFunc("/page2", htmlHandler2)
+	http.HandleFunc("/page3", htmlHandler3)
 
 	// サーバーを起動
 	http.ListenAndServe(":8080", nil)
---
+++ go_webapp_02/templates/template003.html.tpl
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+    Func1 -> {{ samplefunc1 }}<br/>
+    Func2 -> {{ .msg1 | samplefunc2 }}<br/>
+    Func3 -> {{ .msg2 | samplefunc3 }}<br/>
+</body>
+</html>
