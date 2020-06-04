d07f1d886864a5249df411f91c53fa3aa117a94f
linnefromice <linnefromice@gmail.com>
2020-06-04 13:42:31 +0900

    update go_webapp_02 - add view

--- go_webapp_02/main.go
+++ go_webapp_02/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"html/template"
 	"log"
+	"net/http"
 )
+
+func htmlHandler0(w http.ResponseWriter, r *http.Request) {
+	// テンプレートをparse
+	t := template.Must(template.ParseFiles("templates/template000.html.tpl"))
+	str := "Sample Message"
+
+	// テンプレートを描画
+	if err := t.ExecuteTemplate(w, "template000.html.tpl", str);err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	log.Println("Launching...")
+	http.HandleFunc("/page0", htmlHandler0)
+
+	// サーバーを起動
+	http.ListenAndServe(":8080", nil)
 }
