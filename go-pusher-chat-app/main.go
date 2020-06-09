d2fab7d4242fb1816aeee551231ce2d0db0389b4
linnefromice <linnefromice@gmail.com>
2020-06-10 01:28:50 +0900

    update push pj - create main.go

---
+++ go-pusher-chat-app/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	// "encoding/json"
+	// "fmt"
+	// "io/ioutil"
+	"log"
+	// "net/http"
+
+	// pusher "github.com/pusher/pusher-http-go"
+)
+
+func main()  {
+	log.Print("Running...")
+}
