16c2f5e70d926e894ee48e61ed9c5ec95569a73c
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-05 17:24:24 +0900

    update first go-gin - add o/r mapper(gorm) & db(sqlite)

--- fundamental/go-gin-first/go.mod
+++ fundamental/go-gin-first/go.mod
@@ -2,4 +2,8 @@ module linnefromice/go-gin-first
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	gorm.io/driver/sqlite v1.1.1
+	gorm.io/gorm v1.20.0
+)
--- fundamental/go-gin-first/main.go
+++ fundamental/go-gin-first/main.go
@@ -2,16 +2,44 @@ package main
 
 import (
 	"time"
+	"strconv"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"gorm.io/driver/sqlite"
 )
+
+type Todo struct {
+    gorm.Model
+    Text   string
+    Status string
+}
 
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+	  panic("failed to connect database")
+	}
+
+	// db initialize
+	db.AutoMigrate(&Todo{})
+	var todos []Todo
+	db.Order("created_at desc").Find(&todos)
+
+	// add data
+	title := "Homework - " + strconv.Itoa(len(todos) + 1)
+	db.Create(&Todo{Text: title, Status: "DOING"})
+
+	// routing
 	router.GET("/", func(ctx *gin.Context) {
 		request_time := time.Now().Format("2006/01/02 15:04:05.000")
-		ctx.HTML(200, "index.html", gin.H{ "request_time": request_time })
+		db.Order("created_at desc").Find(&todos)
+		ctx.HTML(200, "index.html", gin.H{
+			"request_time": request_time,
+			"todos" : todos,
+		})
 	})
 	router.Run()
 }
