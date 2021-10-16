9e1e04558c4539176b60ef4a9b01bc7311d963c5
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-10-16 15:28:35 +0900

    refactor

--- learn-go-with-tests/basic/arrays/sum.go
+++ learn-go-with-tests/basic/arrays/sum.go
@@ -8,12 +8,12 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func SumAll(numbersToSum ...[]int) (sums []int) {
+func SumAll(numbersToSum ...[]int) []int {
 	lengthOfNumbers := len(numbersToSum)
-	sums = make([]int, lengthOfNumbers)
+	sums := make([]int, lengthOfNumbers)
 
 	for i, numbers := range numbersToSum {
 		sums[i] = Sum(numbers)
 	}
-	return
+	return sums
 }
