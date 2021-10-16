59c127351b912cbf54f23d2374b6ee81d365a05e
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-10-17 00:04:02 +0900

    refactor

--- learn-go-with-tests/basic/pointersAndErrors/wallet_test.go
+++ learn-go-with-tests/basic/pointersAndErrors/wallet_test.go
@@ -3,24 +3,23 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	t.Run("Deposit", func(t *testing.T) {
-		wallet := Wallet{}
-		wallet.Deposit(Bitcoin(20))
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-		want := Bitcoin(20)
 
 		if got != want {
-			t.Errorf("got %d want %d", got, want)
+			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %d want %d", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 }
