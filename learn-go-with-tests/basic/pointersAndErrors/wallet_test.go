03b9633e4414210f9f0ee5ac50e32e0865cd8719
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-10-16 23:59:59 +0900

    update - add method of withdraw

--- learn-go-with-tests/basic/pointersAndErrors/wallet_test.go
+++ learn-go-with-tests/basic/pointersAndErrors/wallet_test.go
@@ -3,12 +3,24 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.Balance()
-	want := Bitcoin(10)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(20))
+		got := wallet.Balance()
+		want := Bitcoin(20)
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }
