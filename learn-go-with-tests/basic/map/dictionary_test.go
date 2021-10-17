8f9bcde88aad374a46167d4ad4453d73ec6fe939
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-10-17 14:39:34 +0900

    refactor

--- learn-go-with-tests/basic/map/dictionary_test.go
+++ learn-go-with-tests/basic/map/dictionary_test.go
@@ -33,3 +33,24 @@ func assertError(t *testing.T, got, want error) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if definition != got {
+		t.Errorf("got %q want %q", got, definition)
+	}
+}
