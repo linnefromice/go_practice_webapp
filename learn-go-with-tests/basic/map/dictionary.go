d66ae0d9e3495e8c14c67cd8a4d66ae3cb192619
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-10-17 14:26:18 +0900

    update

--- learn-go-with-tests/basic/map/dictionary.go
+++ learn-go-with-tests/basic/map/dictionary.go
@@ -4,12 +4,12 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = "could not find the word you were looking for"
+var ErrNotFound = errors.New("could not find the word you were looking for")
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New(ErrNotFound)
+		return "", ErrNotFound
 	}
 	return definition, nil
 }
