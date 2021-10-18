05155eb5f5546b6af6469a960e8e64ee36628d44
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-10-18 15:39:21 +0900

    refactor

--- learn-go-with-tests/basic/select/main.go
+++ learn-go-with-tests/basic/select/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
-		return a
-	} else {
-		return b
+func Racer(a, b string) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
 
 func measureResponseTime(url string) time.Duration {
--- learn-go-with-tests/basic/select/main_test.go
+++ learn-go-with-tests/basic/select/main_test.go
@@ -8,21 +8,19 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0)
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer serverA.Close()
+		defer serverB.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		_, err := Racer(serverA.URL, serverB.URL)
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
-
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
