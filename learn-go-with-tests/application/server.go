fe8e89646f9bfaf2bd2bc5a36858658541ec542f
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-10-19 23:31:44 +0900

    refactor

--- learn-go-with-tests/application/server.go
+++ learn-go-with-tests/application/server.go
@@ -7,18 +7,19 @@ import (
 	"strings"
 )
 
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+	if name == "Floyd" {
+		return "10"
+	}
+	return ""
+}
+
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-
-	if player == "Pepper" {
-		fmt.Fprint(w, "20")
-		return
-	}
-
-	if player == "Floyd" {
-		fmt.Fprint(w, "10")
-		return
-	}
+	fmt.Fprintf(w, GetPlayerScore(player))
 }
 
 func main() {
