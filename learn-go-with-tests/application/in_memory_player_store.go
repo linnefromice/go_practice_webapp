c0112ff177d6a34198a9ae1789a8cf78ffaa96a9
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-10-21 01:25:34 +0900

    update inmemory test

--- learn-go-with-tests/application/in_memory_player_store.go
+++ learn-go-with-tests/application/in_memory_player_store.go
@@ -1,19 +1,27 @@
 package main
 
+import "sync"
+
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
 	return &InMemoryPlayerStore{
 		map[string]int{},
+		sync.RWMutex{},
 	}
 }
 
 type InMemoryPlayerStore struct {
 	store map[string]int
+	lock  sync.RWMutex
 }
 
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.lock.Lock()
+	defer i.lock.Unlock()
 	return i.store[name]
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.lock.RLock()
+	defer i.lock.RUnlock()
 	i.store[name]++
 }
