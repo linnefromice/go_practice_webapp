d78ed1216dd200fbae5d1c230eee85b03a8e5aa3
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-10-23 01:04:54 +0900

    refactor

--- go_api_01/internal/pkg/models/task_test.go
+++ go_api_01/internal/pkg/models/task_test.go
@@ -21,6 +21,9 @@ func TestTask(t *testing.T) {
 		if task.isFinished != false {
 			t.Error("isFinished is wrong")
 		}
+		if task.isFinished != false {
+			t.Error("isFinished is wrong")
+		}
 		if task.version != 1 {
 			t.Error("version is wrong")
 		}
