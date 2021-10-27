025de1e908d96bae94c8749686ee702fd00d46f4
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-10-27 09:14:21 +0900

    implement logic of api handler (dummy logic)

---
+++ go_api_01/internal/pkg/oas-api/server.go
@@ -0,0 +1,36 @@
+package openapi
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type OasServerImpl struct{}
+
+func NewDummyTask() Task {
+	return Task{
+		Description: "Description",
+		Id:          1,
+		IsDeleted:   false,
+		IsFinished:  false,
+		Title:       "Title",
+		Version:     1,
+	}
+}
+
+func (s OasServerImpl) PostTask(ctx echo.Context) error {
+	return ctx.JSON(http.StatusOK, NewDummyTask())
+}
+
+func (s OasServerImpl) DeleteTaskTaskId(ctx echo.Context, taskId string) error {
+	return ctx.JSON(http.StatusOK, NewDummyTask())
+}
+
+func (s OasServerImpl) GetTasksTaskId(ctx echo.Context, taskId string) error {
+	return ctx.JSON(http.StatusOK, NewDummyTask())
+}
+
+func (s OasServerImpl) PatchTasksTaskId(ctx echo.Context, taskId string) error {
+	return ctx.JSON(http.StatusOK, NewDummyTask())
+}
